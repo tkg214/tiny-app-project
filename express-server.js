7852d04fa84159f210845bc6bfa1c2903ebb70b8
Ken <tkg214@gmail.com>
2017-02-15 02:43:29 +0000

    Add form post, redirect to express-server.js

--- express-server.js
+++ express-server.js
@@ -1,26 +1,60 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 
-app.set("view engine", "ejs")
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+const randomstring = require('randomstring')
+
+app.set('view engine', 'ejs')
+
+let urlDatabase = {
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
+app.get('/', (req, res) => {
+  res.end('Hello!');
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// receives request to show specific url page and responds with .ejs template
+app.get('/urls', (req, res) => {
+  res.render('urls-index', { urls: urlDatabase });
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+// receives request to show specific url page and responds with .ejs template, otherwise responds with form
+app.get('/urls/:id', (req, res) => {
+  if (req.params.id in urlDatabase) {
+    res.render('urls-show', {
+      key: req.params.id,
+      url: urlDatabase[req.params.id]
+    });
+    } else {
+      res.render('urls-new');
+    }
 });
+
+// redirects to long URL
+app.get('/u/:shortURL', (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+// receives form post and redirects to new page created
+app.post('/urls', (req, res) => {
+  let newShortURL = generateRandomString(6);
+  urlDatabase[newShortURL] = req.body.longURL;
+  res.redirect(`/urls/${newShortURL}`);
+});
+
+// generates random string using randomstring module
+function generateRandomString(length) {
+  return randomstring.generate(length);
+};
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
