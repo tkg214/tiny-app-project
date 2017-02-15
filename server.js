8efbc92fd023cd30e68f1aa3e8804c8ce696f0e7
Ken <tkg214@gmail.com>
2017-02-15 22:13:27 +0000

    Add login and logout features

--- server.js
+++ server.js
@@ -5,22 +5,30 @@ const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-const randomstring = require('randomstring')
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
-app.set('view engine', 'ejs')
+const randomstring = require('randomstring');
+
+app.set('view engine', 'ejs');
 
 let urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+
+// receives request for root path, redirects to /urls
 app.get('/', (req, res) => {
-  res.end('Hello!');
+  res.redirect('/urls');
 });
 
 // receives request to show list of urls page and responds with rendered urls_index.ejs
 app.get('/urls', (req, res) => {
-  res.render('urls_index', { urls: urlDatabase });
+  res.render('urls_index', {
+    urls: urlDatabase,
+    username: req.cookies.username
+  });
 });
 
 // receives request to show specific url page and responds with rendered urls_show.ejs template, otherwise responds with urls_new.ejs
@@ -28,10 +36,11 @@ app.get('/urls/:shortURL', (req, res) => {
   if (req.params.shortURL in urlDatabase) {
     res.render('urls_show', {
       key: req.params.shortURL,
-      url: urlDatabase[req.params.shortURL]
+      url: urlDatabase[req.params.shortURL],
+      username: req.cookies.username
     });
     } else {
-      res.render('urls_new');
+      res.render('urls_new', { username: req.cookies.username });
     }
 });
 
@@ -60,6 +69,18 @@ app.post('/urls/:shortURL', (req, res) => {
     urlDatabase[req.params.shortURL] = req.body.longURL;
   }
   res.redirect('/urls');
+});
+
+// receives form post request to sign in, responds with cookie and redirection to /
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/');
+});
+
+// receives form post request to sign out, responds with cookie deletion and redirection to /
+app.post('/logout', (req, res) => {
+  res.clearCookie('username')
+  res.redirect('/');
 });
 
 
